aede8b1a3137cf6923179ceb15f6b755c4077cca
lonepu <mgpumgpu.1997@gmail.com>
2025-07-05 17:22:17 +0630

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('LonePu'));
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('The Title')),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text(
+                'Answer 1',
+              ), // Replace null with a function to enable the button
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print('Answer 3 chosen! '),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
