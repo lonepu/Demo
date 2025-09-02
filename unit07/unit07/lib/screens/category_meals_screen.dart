1800c762dea016f391ba86e5ec401f5673613d2b
lonepu <mgpumgpu.1997@gmail.com>
2025-09-02 17:03:07 +0630

    Display Recipe Items & Using Network Images

--- unit07/unit07/lib/screens/category_meals_screen.dart
+++ unit07/unit07/lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './dummy_data.dart';
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routName = '/category-meals';
@@ -23,7 +24,13 @@ class CategoryMealsScreen extends StatelessWidget {
       ), //‌as String က ကိုယ့်ဘာသာကိုယ်ထည့်တာပါ error တက်နေလို့ပါ
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
         },
         itemCount: categoryMeals.length,
       ),
