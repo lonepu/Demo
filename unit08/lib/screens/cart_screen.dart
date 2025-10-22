d42eded2ed10eeb3f9eb0e2e81d8c5f7d69ecefe
lonepu <mgpumgpu.1997@gmail.com>
2025-10-22 14:58:04 +0630

    add final part (finish)

--- unit08/lib/screens/cart_screen.dart
+++ unit08/lib/screens/cart_screen.dart
@@ -28,7 +28,7 @@ class CartScreen extends StatelessWidget {
                   Spacer(),
                   Chip(
                     label: Text(
-                      '\$${cart.totalAmount}',
+                      '\$${cart.totalAmount.toStringAsFixed(2)}',
                       style: TextStyle(
                         color: Theme.of(context).colorScheme.secondary,
                       ),
--- unit08/lib/widgets/order_item.dart
+++ unit08/lib/widgets/order_item.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:intl/intl.dart';
 import '../providers/orders.dart'
     as ord; // Import Orders provider as ord to avoid name conflict
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItems order;
 
   OrderItem(this.order);
 
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,19 +22,53 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             // Display total amount of the order
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm ').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm ').format(widget.order.dateTime),
               // Format date and time using package:intl
             ),
             trailing: IconButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
               // Function when pressed to expand/collapse order details
-              icon: Icon(Icons.expand_more),
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
               // Icon can be changed to expand_less when expanded
             ),
           ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.products.length * 20.0 + 100, 180),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${prod.quantity}x \$${prod.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
